597e1af68105437f1d2e7cd493d7037e6614ca2d
Jessica Stringham <jessstringham@users.noreply.github.com>
2014-02-03 22:25:36 -0800

    Fixed naming.

--- src/backend/Main.hs
+++ src/backend/Main.hs
@@ -5,12 +5,12 @@ import DataStructures
 import System.Environment
 
 
---checkEither :: Eq a => Either String a -> a -> String
+checkEither :: Either [Char] t -> t
 checkEither (Left err)= error err
 checkEither (Right dataThing) = dataThing
 
-test :: IO ()
-test = do
+main :: IO ()
+main = do
     book_location:_ <- getArgs
     yamlData <- BS.readFile book_location
     let stuff = Data.Yaml.decodeEither yamlData :: Either String Book
